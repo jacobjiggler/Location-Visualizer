3762a796f7bb95ac636b182a012038f77041d1f1
jacobjiggler <jmart.alb@gmail.com>
2016-02-15 21:29:59 -0500

    Wrote CSV Wrapper. Will test in a sec.

--- utils.py
+++ utils.py
@@ -1,3 +1,6 @@
+import csv
+import sys
+
 class Point:
     def __init__(self, lat, lon, time, acc):
         self.lat = lat
@@ -5,6 +8,21 @@ class Point:
         self.time = time
         self.acc = acc
 
+class CsvWrapper:
+	def __init__(self, file):
+		with open(file) as csvfile:
+			self.points = []
+			self.itr = 0
+			readCSV = csv.reader(csvfile, delimiter=',')
+			for row in readCSV:
+				if (len(row) != 4):
+					print("error")
+				else:
+					points.append(Point(row[0],row[1],row[2],row[3]))
+
+	def getPoint():
+		itr+=1
+		return points[itr-1]
 
 def lerp(a, b, time):
 	"""
