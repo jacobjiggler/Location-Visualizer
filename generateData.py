ec1b47da562a827f77c0f0d2f81b5c00f5721acc
jacobjiggler <jmart.alb@gmail.com>
2016-02-15 20:50:05 -0500

    Basic stuff while waiting for rob's commit.

--- generateData.py
+++ generateData.py
@@ -8,14 +8,23 @@ school_long_min = -73.684845
 school_long_max = -73.671747
 
 
-
 def atSchool(latitude, longitude):
     if (latitude > school_lat_min and latitude < school_lat_max):
         if (longitude > school_long_min and longitude < school_long_max):
             return True
     return False
 
+
 #print(atSchool(42.729320, -73.680688))
+
+
+def analyzeLocationData(csv):
+    schoolCount = 0
+    crowsCount = 0
+    officeCount = 0
+
+def analyzeMeetups(csv1,csv2):
+    meetingPoints = []
 
 
 if __name__ == '__main__':
@@ -24,8 +33,11 @@ if __name__ == '__main__':
     #argv[1] is the first csv file
     #argv[2] is optional and the second csv if you want to compare
     if (len(sys.argv) == 2):
-        print("processing campus data")
+        print("processing location data")
+        analyzeLocationData(sys.argv[1])
+
     elif(len(sys.argv) == 3):
         print("comparing 2 datasets")
+        analyzeMeetups(sys.argv[1], sys.argv[2])
     else:
         print("usage: python generateData csv1 [optional csv2 for comparison]")
